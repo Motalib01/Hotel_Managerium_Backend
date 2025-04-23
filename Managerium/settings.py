d2bb3d1beabd3dca54181b836461250f0e87cb63
Motalib01 <abdelmotalib.chemoouri@gmail.com>
2025-04-23 10:34:41 +0100

    change to postgres

--- Managerium/settings.py
+++ Managerium/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'Managerium.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'managerium-db',        # Replace with your PostgreSQL DB name
+        'USER': 'postgres',        # Replace with your PostgreSQL username
+        'PASSWORD': '12345',# Replace with your PostgreSQL password
+        'HOST': 'localhost',           # Default: localhost (or use the IP address if needed)
+        'PORT': '5432',                # Default PostgreSQL port
     }
 }
 
